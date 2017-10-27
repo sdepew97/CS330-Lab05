e0f7945387f724c7d9b6101a4360e60580afa45f
Arthur <Arthur@Arthur>
2017-10-26 21:48:39 -0400

    First couple of lines and testing commit.

---
+++ CS340-Programming-Project/src/cs340/programming/project/CS340ProgrammingProject.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs340.programming.project;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Arthur
+ */
+public class CS340ProgrammingProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File constraints = new File("haverfordConstraints.txt");
+        File student_prefs = new File("haverfordStudentPrefs.txt");
+        Scanner constraints_scanner = new Scanner(constraints);
+        Scanner student_prefs_scanner = new Scanner(student_prefs);
+    }
+    
+}
