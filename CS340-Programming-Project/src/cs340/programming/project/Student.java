29ffae0a794a79baa0e1639d6f9a3a9a863e1a8a
Arthur <Arthur@Arthur>
2017-10-27 21:39:24 -0400

    Unstashing

--- CS340-Programming-Project/src/cs340/programming/project/Student.java
+++ CS340-Programming-Project/src/cs340/programming/project/Student.java
@@ -6,13 +6,13 @@ public class Student {
     //data fields
     private int studentID;
     private ArrayList<Integer> preferencesList;
-    private ArrayList<Class> enrolledClassList;
+    private ArrayList<Integer> enrolledClassList;
 
     public Student(int studentID, ArrayList<Integer> preferencesList)
     {
         this.studentID = studentID;
         this.preferencesList = preferencesList;
-        this.enrolledClassList = new ArrayList<Class>();
+        this.enrolledClassList = new ArrayList<Integer>();
     }
 
     //getter methods
@@ -26,7 +26,7 @@ public class Student {
         return this.preferencesList;
     }
 
-    public ArrayList<Class> getEnrolledClassList()
+    public ArrayList<Integer> getEnrolledClassList()
     {
         return this.enrolledClassList;
     }
@@ -42,7 +42,7 @@ public class Student {
         this.preferencesList = preferencesList;
     }
 
-    public void setEnrolledClassList(ArrayList<Class> enrolledClassList)
+    public void setEnrolledClassList(ArrayList<Integer> enrolledClassList)
     {
         this.enrolledClassList = enrolledClassList;
     }
